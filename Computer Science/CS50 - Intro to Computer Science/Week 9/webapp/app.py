9861caaace3692ba4e987380c21c96ce609c6bb6
rozran00@gmail.com <rozran00@gmail.com>
2022-06-08 12:47:37 -0400

    finished CS50 course

--- Computer Science/CS50 - Intro to Computer Science/Week 9/webapp/app.py
+++ Computer Science/CS50 - Intro to Computer Science/Week 9/webapp/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/greet")
+@app.route("/greet", methods=["POST"])
 def greet():
-    name = request.args.get("name")
+    name = request.form.get("name")
     return render_template("greet.html", name = name)
