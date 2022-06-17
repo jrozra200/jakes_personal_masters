d53e8663837852099f035eb88128c3ddc4857a86
rozran00@gmail.com <rozran00@gmail.com>
2022-06-17 15:02:01 -0400

    Not sure how to implement this - putting it on ice for now

---
+++ AI/CS50 - Intro to AI/Week 1/knights/puzzle.py
@@ -0,0 +1,121 @@
+from logic import *
+
+AKnight = Symbol("A is a Knight")
+AKnave = Symbol("A is a Knave")
+a_claim_knight = Symbol("A Claims Knight")
+a_claim_knave = Symbol("A Claims Knave")
+statement_a = Symbol("A Claims")
+
+BKnight = Symbol("B is a Knight")
+BKnave = Symbol("B is a Knave")
+b_claim_knight = Symbol("B Claims Knight")
+b_claim_knave = Symbol("B Claims Knave")
+statement_b = Symbol("B Claims")
+
+CKnight = Symbol("C is a Knight")
+CKnave = Symbol("C is a Knave")
+
+# Puzzle 0
+# A says "I am both a knight and a knave."
+knowledge0 = And(
+    
+    # You have to be a knight or a knave
+    Or(AKnight, AKnave),
+    # But you can't be both
+    Or(
+        And(AKnight, Not(AKnave)), 
+        And(Not(AKnight), AKnave)
+    )
+    
+    # If your statement is true, you are a knight
+    
+    # If your statement is false, you are a knight
+    
+    # A says "I am both a knight and a knave."
+    a_claim_knave,
+    a_claim_knight,
+    
+    # YOU CAN SAY THE FOLLOWING:
+    # "I am a knight" -> Can be true of false
+    Implication(a_claim_knight, Or(a_claim_knight, a_claim_knight)), 
+    # "I am a knave" -> Cannot be true -> cannot be said
+    
+    # "I am both a knight and a knave." -> Cannot be true 
+    Implication(And(a_claim_knight, a_claim_knave), Not(statement_a)),
+    # "I am neither a knight or a knave." -> Cannot be true 
+    Implication(Not(Or(a_claim_knight, a_claim_knave)), Not(statement_a)), # DIDN'T SAY THIS
+    
+    # If statement_a is true, you are a knight
+    Implication(statement_a, And(AKnight, Not(AKnave))),
+    # If statement_a is false, you are a knave
+    Implication(Not(statement_a), And(Not(AKnight), AKnave))
+)
+
+# Puzzle 1
+# A says "We are both knaves."
+# B says nothing.
+knowledge1 = And(
+    
+    # A says "We are both knaves."
+    a_claim_knave,
+    b_claim_knave,
+    
+    # YOU CAN SAY THE FOLLOWING:
+    # "I am a knight" -> Can be true of false
+    Implication(a_claim_knight, Or(a_claim_knight, a_claim_knight)), 
+    # "I am a knave" -> Cannot be true -> cannot be said
+    
+    # "I am both a knight and a knave." -> Cannot be true 
+    Implication(And(a_claim_knight, a_claim_knave), Not(statement_a)),
+    # "I am neither a knight or a knave." -> Cannot be true 
+    Implication(Not(Or(a_claim_knight, a_claim_knave)), Not(statement_a)), # DIDN'T SAY THIS
+    
+    # If statement_a is true, you are a knight
+    Implication(statement_a, And(AKnight, Not(AKnave))),
+    # If statement_a is false, you are a knave
+    Implication(Not(statement_a), And(Not(AKnight), AKnave))
+)
+
+symbols = [AKnight, AKnave]
+
+for symbol in symbols:
+    if model_check(knowledge0, symbol):
+        print(f"    {symbol}")
+
+# Puzzle 2
+# A says "We are the same kind."
+# B says "We are of different kinds."
+knowledge2 = And(
+    # TODO
+)
+
+# Puzzle 3
+# A says either "I am a knight." or "I am a knave.", but you don't know which.
+# B says "A said 'I am a knave'."
+# B says "C is a knave."
+# C says "A is a knight."
+knowledge3 = And(
+    # TODO
+)
+
+
+def main():
+    symbols = [AKnight, AKnave, BKnight, BKnave, CKnight, CKnave]
+    puzzles = [
+        ("Puzzle 0", knowledge0),
+        ("Puzzle 1", knowledge1),
+        ("Puzzle 2", knowledge2),
+        ("Puzzle 3", knowledge3)
+    ]
+    for puzzle, knowledge in puzzles:
+        print(puzzle)
+        if len(knowledge.conjuncts) == 0:
+            print("    Not yet implemented.")
+        else:
+            for symbol in symbols:
+                if model_check(knowledge, symbol):
+                    print(f"    {symbol}")
+
+
+if __name__ == "__main__":
+    main()
